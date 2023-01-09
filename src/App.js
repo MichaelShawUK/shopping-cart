ebacc4979ffbca97e8c076f0cb5c0ccd9b9f17f4
MichaelShawUK <MichaelShawUK@gmail.com>
2023-01-09 23:52:04 +0000

    Style homepage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ const App = () => {
   return (
     <div className="App">
       <div id="home-main"></div>
+      <div id="slide-left"></div>
+      {/* <div class="flex-main">
+        <div className="main-content">Unlock your guitar sound</div>
+        <Link to="/shop">
+          <div className="main-content">ENTER SHOP</div>
+        </Link>
+      </div> */}
     </div>
   );
 };
