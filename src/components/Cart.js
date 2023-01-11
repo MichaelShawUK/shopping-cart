1786cfa7a922704c2cdd1d7366d7e1785cdf2765
MichaelShawUK <MichaelShawUK@gmail.com>
2023-01-11 23:33:02 +0000

    Display empty cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,43 +5,46 @@ const Cart = ({ order, changeQuantity }) => {
     (sum, item) => sum + item.price * item.quantity,
     0
   );
-  console.log(total);
 
-  return (
-    <div className="Cart">
-      {order.map((item) => {
-        return (
-          <div className="cart-item" key={item.id}>
-            <div className="item-info">
-              <div className="cart-name">{item.name}</div>
-              <div className="cart-price">{`Price: £${item.price}`}</div>
-              <div className="cart-quantity">
-                <button
-                  className="quantity-btn"
-                  data-id={item.id}
-                  data-count="decrement"
-                  onClick={changeQuantity}
-                >
-                  -
-                </button>
-                <button
-                  className="quantity-btn"
-                  data-id={item.id}
-                  data-count="increment"
-                  onClick={changeQuantity}
-                >
-                  +
-                </button>
-                {`Quantity: ${item.quantity}`}
+  {
+    return total ? (
+      <div className="Cart">
+        {order.map((item) => {
+          return (
+            <div className="cart-item" key={item.id}>
+              <div className="item-info">
+                <div className="cart-name">{item.name}</div>
+                <div className="cart-price">{`Price: £${item.price}`}</div>
+                <div className="cart-quantity">
+                  <button
+                    className="quantity-btn"
+                    data-id={item.id}
+                    data-count="decrement"
+                    onClick={changeQuantity}
+                  >
+                    -
+                  </button>
+                  <button
+                    className="quantity-btn"
+                    data-id={item.id}
+                    data-count="increment"
+                    onClick={changeQuantity}
+                  >
+                    +
+                  </button>
+                  {`Quantity: ${item.quantity}`}
+                </div>
               </div>
+              <img className="item-img" src={item.img} alt=""></img>
             </div>
-            <img className="item-img" src={item.img} alt=""></img>
-          </div>
-        );
-      })}
-      {total ? <div id="order-total">Order Total: {`£${total}`}</div> : null}
-    </div>
-  );
+          );
+        })}
+        {total ? <div id="order-total">Order Total: {`£${total}`}</div> : null}
+      </div>
+    ) : (
+      <div id="empty-cart">Your cart is empty</div>
+    );
+  }
 };
 
 export default Cart;
